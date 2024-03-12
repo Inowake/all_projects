c8f12343b80c0d384b2a9b2f90ec23cbd0e12ad8
Inowake <136159389+Inowake@users.noreply.github.com>
2024-03-12 22:53:22 +0300

    Update Spambot v1.py
    added color to buttons and more functionality

--- Spam Bot v1/Spambot v1.py
+++ Spam Bot v1/Spambot v1.py
@@ -5,19 +5,19 @@ import time
 window = Tk()
 window.geometry("300x600")
 window.resizable(False, False)
-window.title("Mal deronder")
+window.title("Spam Bot v1.1")
 
-text = "Spam Bot v1"
+text = "Spam Bot v1.1"
 def bas():
-    # text = "43"
-    # metin.pack(side=BOTTOM)
-    # buton.configure(text="mega zeki deren", font=("Cooper Black", 30))
-    time.sleep(1)
     x = 0
     y = int(kackre.get())
+    z = float(kacms.get())
+    t = float(kacms2.get())
+    time.sleep(t)
+
     while x<y:
         keyboard.write(giris.get())
-        time.sleep(0.2)
+        time.sleep(z)
         keyboard.press_and_release('Enter')
         x += 1
 
@@ -30,19 +30,25 @@ def max():
     window.geometry(newGeometry="1920x1080+0+0")
 
 baslik = Label(window, text=text, font=("Bauhaus 93",25))
-buton = Button(window, text="spamla", font=("Colonna MT", 20),command=bas , height=10, width=10)
-metin = Label(window, text="muaaahhahahha")
+buton = Button(window, text="spam", font=("Colonna MT", 20),command=bas , height=10, width=10)
 butonkapa = Button(window, text="⨉",font=("Arial", 15), command=kapa)
 butonmax = Button(window, text="◻",font=("Arial", 15), command=max)
-giris = Entry(window, text="varsayılamayan metin", font=("Dubai", 10))
-kackre = Entry(window, text="ne", font=("Dubai", 10),)
+giris = Entry(window,  font=("Dubai", 10), background="red")
+kackre = Entry(window, font=("Dubai", 10),background="blue")
+kacms = Entry(window,  font=("Dubai", 10),background="green")
+kacms2 = Entry(window,  font=("Dubai", 10),background="yellow")
+info = Label(window, text="red: text, blue: repeat count\n green: spam cooldown, yellow: time to start", font=("Calibri", 10))
 
-# baslik.place(x=0 ,y=0)
+
 baslik.pack(pady=50)
 buton.pack(pady=10)
 butonkapa.place(x=10, y=10)
 butonmax.place(x=50, y=10)
 giris.pack()
 kackre.pack()
+kacms.pack()
+kacms2.pack()
+info.pack()
+
 
 window.mainloop()
